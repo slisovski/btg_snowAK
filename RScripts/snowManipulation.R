414a23edc788664e1c714656d2a7c1b88a0519c4
Simeon Lisovski <slisovsk@linux4.awi-potsdam.de>
2020-10-10 09:30:26 +0200

    snowRaw

--- RScripts/snowManipulation.R
+++ RScripts/snowManipulation.R
@@ -7,7 +7,7 @@ btg <- st_read("data/btg_breedingAK/btg_breedingSiteAK.shp") %>% st_set_crs(4326
 plot(btg$geometry)
 
 
-fls.gz <- list.files("/Users/slisovsk/Desktop/4km", pattern = ".asc.gz", recursive = T,  full.names = T)
+fls.gz <- list.files("/bioing/user/slisovsk/4km/", pattern = ".asc.gz", recursive = T,  full.names = T)
 dates  <- as.Date(as.POSIXct(unlist(lapply(strsplit(fls.gz, "ims"), function(x) strsplit(x[[2]], "_4km")))[c(TRUE, FALSE)], format = "%Y%j"))
 
 prj <- "+proj=stere +lat_0=90 +lat_ts=60 +lon_0=-80 +k=1 +x_0=0 +y_0=0 +a=6378137 +b=6356257 +units=m +no_defs"
@@ -31,7 +31,7 @@ prj <- "+proj=stere +lat_0=90 +lat_ts=60 +lon_0=-80 +k=1 +x_0=0 +y_0=0 +a=637813
   S01_1 <- crop(S01, as(btg %>% st_transform(CRS(prj)) %>% st_geometry(), "Spatial"))
   plot(S01_1)
   plot(btg %>% st_transform(CRS(prj)) %>% st_geometry(), add = T, col = "transparent")
-  crdsP <- coordinates(S02)[unlist(extract(S02, as(btg %>% st_transform(CRS(prj)) %>% st_geometry(), "Spatial"))),]
+  crdsP <- coordinates(S02)[unlist(raster::extract(S02, as(btg %>% st_transform(CRS(prj)) %>% st_geometry(), "Spatial"))),]
   crds  <- project(crdsP, proj = prj, inv = T) 
   ###### ----
 
@@ -44,10 +44,10 @@ for(i in 1:length(dates)) {
                 i, length(dates)))
     
     tab0 <- readLines(fls.gz[i])
-    ind <- unlist(suppressWarnings(lapply(tab0, function(x) is.na(as.numeric(gsub(" ", "", x))))))
+    ind <- unlist(suppressWarnings(parallel::mclapply(tab0, function(x) is.na(as.numeric(gsub(" ", "", x))), mc.cores = 15)))
     tab <- tab0[-which(ind)]
     
-    z = unlist(lapply(tab, function(.line) as.numeric(strsplit(.line, '')[[1]])))
+    z = unlist(parallel::mclapply(tab, function(.line) as.numeric(strsplit(.line, '')[[1]]), mc.cores = 12))
     m <- matrix(z, ncol = 6144, nrow = 6144, byrow = T)
     m   <- m[nrow(m):1,]
     S01 <- raster(m)
@@ -55,7 +55,7 @@ for(i in 1:length(dates)) {
     extent(S01) <- c(-12288000, 12288000, -12288000, 12288000)
     proj4string(S01) <- prj
 
-    snowM[,i] <- unlist(extract(S01, crdsP))
+    snowM[,i] <- unlist(raster::extract(S01, crdsP))
     
 }
 snowRaw <- list(crds = crds, dates = dates, snow = snowM)
