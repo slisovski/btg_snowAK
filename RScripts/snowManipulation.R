72709f1ceb92b1b06999ccf095743d92ae2bd3c3
Simeon Lisovski <slisovsk@linux4.awi-potsdam.de>
2020-10-12 08:02:31 +0200

    LONGTS2020

--- RScripts/snowManipulation.R
+++ RScripts/snowManipulation.R
@@ -7,8 +7,9 @@ btg <- st_read("data/btg_breedingAK/btg_breedingSiteAK.shp") %>% st_set_crs(4326
 plot(btg$geometry)
 
 
-fls.gz <- list.files("/bioing/user/slisovsk/4km/", pattern = ".asc.gz", recursive = T,  full.names = T)
+fls.gz <- list.files("/bioing/user/slisovsk/4km/", pattern = ".gz", recursive = T,  full.names = T)
 dates  <- as.Date(as.POSIXct(unlist(lapply(strsplit(fls.gz, "ims"), function(x) strsplit(x[[2]], "_4km")))[c(TRUE, FALSE)], format = "%Y%j"))
+plot(dates)
 
 prj <- "+proj=stere +lat_0=90 +lat_ts=60 +lon_0=-80 +k=1 +x_0=0 +y_0=0 +a=6378137 +b=6356257 +units=m +no_defs"
 
@@ -36,18 +37,18 @@ prj <- "+proj=stere +lat_0=90 +lat_ts=60 +lon_0=-80 +k=1 +x_0=0 +y_0=0 +a=637813
   ###### ----
 
 
-snowM <- matrix(ncol = length(dates), nrow = nrow(crds))
-  
-for(i in 1:length(dates)) {
+library(pbmcapply)  
+
+snowM <- do.call("cbind", pbmclapply(1:length(dates), function(x) {  
+      
+    # cat(sprintf('\rDate %d of %d',
+                # i, length(dates)))
     
-    cat(sprintf('\rDate %d of %d',
-                i, length(dates)))
+    tab0 <- suppressWarnings(readLines(fls.gz[i]))
+    ind  <- unlist(suppressWarnings(lapply(tab0, function(x) is.na(as.numeric(gsub(" ", "", x))))))
+    tab  <- tab0[-which(ind)]
     
-    tab0 <- readLines(fls.gz[i])
-    ind <- unlist(suppressWarnings(parallel::mclapply(tab0, function(x) is.na(as.numeric(gsub(" ", "", x))), mc.cores = 15)))
-    tab <- tab0[-which(ind)]
-    
-    z = unlist(parallel::mclapply(tab, function(.line) as.numeric(strsplit(.line, '')[[1]]), mc.cores = 12))
+    z = unlist(lapply(tab, function(.line) as.numeric(strsplit(.line, '')[[1]])))
     m <- matrix(z, ncol = 6144, nrow = 6144, byrow = T)
     m   <- m[nrow(m):1,]
     S01 <- raster(m)
@@ -55,8 +56,10 @@ for(i in 1:length(dates)) {
     extent(S01) <- c(-12288000, 12288000, -12288000, 12288000)
     proj4string(S01) <- prj
 
-    snowM[,i] <- unlist(raster::extract(S01, crdsP))
+    unlist(raster::extract(S01, crdsP))
     
-}
+}, mc.cores = parallel::detectCores()-10))
+  
+
 snowRaw <- list(crds = crds, dates = dates, snow = snowM)
-save(snowRaw, file = "Results/snowRaw_4km.RData")
+save(snowRaw, file = "Results/snowRaw_4km_2004_2020.RData")
